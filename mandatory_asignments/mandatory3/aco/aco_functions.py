6ca668cf40308b1177c43db033594257d8f59e51
carlos reiakvam <cre032@uit.no>
2022-10-01 13:17:09 +0200

    daily updates

--- mandatory_asignments/mandatory3/aco/aco_functions.py
+++ mandatory_asignments/mandatory3/aco/aco_functions.py
@@ -1,7 +1,7 @@
 import random
 
+__author__ = 'Carlos Reiakvam <cre032@uit.no>'
 
-# Author: Carlos Reiakvem, october 2022
 
 def gen_pheromone_matrix(graph: dict) -> list:
     pheromone_matrix = []
@@ -18,19 +18,18 @@ def get_random_edge(graph: dict, from_node):
     edges: dict = graph[str(from_node)]
     random_edge: int = int((random.choice(list(edges))))
     random_edge_n = list(edges.keys()).index(str(random_edge))
-
     return random_edge_n
 
 
-def get_to_node(from_node: int, graph: dict):
+def get_to_node(from_node : int, graph: dict) -> list:
     edges: dict = graph[str(from_node)]
-    to_node: int = int((random.choice(list(edges))))
-    to_node_n = list(edges.keys()).index(str(to_node))
-    return int(to_node_n)
+    to_node_n: int = int((random.choice(list(edges))))  # number of to_node
+    to_node_i = list(edges.keys()).index(str(to_node_n))  # index of to_node
+    return [to_node_n, to_node_i]
 
 
 def update_pheromone(from_node, to_node, pheromone_matrix, graph):
-    to_node = str(to_node)
-    weight = graph[str(from_node)][to_node]['weight']
+    weight = graph[str(from_node)][str(to_node)]['weight']
+
     # update pheremone matrix
-    pheromone_matrix[from_node][to_node] += 0.001 * 1 / weight
+    pheromone_matrix[int(from_node)][int(to_node)] += 0.001 * 1 / weight
