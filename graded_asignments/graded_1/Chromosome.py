784b62efbe6cb1748835dc124e859c8e920de28a
carlos reiakvam <cre032@uit.no>
2022-10-16 19:52:48 +0200

    daily updates

--- graded_asignments/graded_1/Chromosome.py
+++ graded_asignments/graded_1/Chromosome.py
@@ -3,23 +3,35 @@ import math as m
 import numpy as np
 
 
-class Chromosome:
+class Features:
     def __init__(self):
-        # initial values of first generation are created randomly
-        # they are represented as binary numbers
-        self.it = 870  # Ideal thrust: 870N
         self.a = bin(randint(0, 45))  # blade angle
         self.b = bin(choice(np.array([2, 3, 4, 5])))  # n blades
         self.y = bin(randint(55, 75))  # air/fuel ratio, represented by * 0.01
         self.d = bin(randint(1000, 2000))  # propeller diameter
         self.t = bin(randint(5, 50))  # , idle valve position theta, represented by * 0.01
-        self.fitness_val = 0
 
-    def thrust(self, a, b, y, d, t):
-        return (m.pow(a, b) + m.log(y)) / (d + m.pow(t, 3))
 
-    def fitness(self, a, b, y, d, t):
-        self.fitness_val = self.it - self.thrust(int(a, 2), int(b, 2), int(y, 2), int(d, 2), int(t, 2))
+class Chromosome:
+    def __init__(self, chromo_id):
+        self.id = chromo_id
+        self.it = 870  # Ideal thrust: 870N
+        self.ftr = Features()
+        self.fitness_val = float('inf')
+
+    def get_mapped(self):
+        return {'a': int(self.ftr.a, 2),
+                'b': int(self.ftr.b, 2),
+                'y': int(self.ftr.y, 2),
+                'd': int(self.ftr.d, 2),
+                't': int(self.ftr.y, 2)}
+
+    def get_thrust(self):
+        mv = self.get_mapped()
+        return (m.pow(mv['a'], mv['b']) + m.log(mv['y'])) / (mv['d'] + m.pow(mv['t'], 3))
+
+    def set_fitness(self):
+        self.fitness_val = 1 / (self.it - self.get_thrust())
 
     def get_fitness(self):
         return self.fitness_val
@@ -31,3 +43,14 @@ class Chromosome:
     def mutate(self):
         pass
         # mutate features to avoid getting studck in local mimima
+
+    def __str__(self):
+        mv = self.get_mapped()
+        return str(f'Chromosome {self.id}\n'
+                   f"a:{mv['a']}\n"
+                   f"b:{mv['b']}\n"
+                   f"y:{mv['y']}\n"
+                   f"d:{mv['d']}\n"
+                   f"t:{mv['t']}\n"
+                   f'thrust: {self.get_thrust()}\n'
+                   f'fitness: {self.fitness_val}\n')
--- graded_asignments/graded_1/main.py
+++ graded_asignments/graded_1/main.py
@@ -1,15 +1,24 @@
 from graded_asignments.graded_1.Chromosome import Chromosome
 
 if __name__ == '__main__':
-    c = Chromosome()
-
+    # c = Chromosome()
     # a = c.fitness(c.a, c.b, c.y, c.d, c.t)
     # a = int(c.a, 2)
     # b = int(c.b, 2)
     # y = int(c.y, 2)
     # d = int(c.y, 2)
     # t = int(c.t, 2)
+    # print(c.get_fitness())
+    # a = c.fitness(c.a, c.b, c.y, c.d, c.t)
+    # print(c.get_fitness())
 
-    print(c.get_fitness())
-    a = c.fitness(c.a, c.b, c.y, c.d, c.t)
-    print(c.get_fitness())
+    population = []
+    pop_size = 10
+    for i in range(pop_size):
+        population.append(Chromosome(i))
+
+    for p in population:
+        p.set_fitness()
+
+    for p in population:
+        print(p)
