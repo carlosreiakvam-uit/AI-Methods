98d4e3d684d4558355725f81f86e56ab658adb8c
carlos reiakvam <cre032@uit.no>
2022-11-16 10:58:58 +0100

    daily updates

--- graded_asignments/graded_1/genetic_algorithm.py
+++ graded_asignments/graded_1/genetic_algorithm.py
@@ -1,7 +1,7 @@
 import random
 import numpy as np
 import math
-from course.graded_asignments.graded_1.chromosome import Chromosome
+from graded_asignments.graded_1.chromosome import Chromosome
 from constants import *
 
 
